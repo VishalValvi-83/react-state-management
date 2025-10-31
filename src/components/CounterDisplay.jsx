4d282007690ee6db6e48075d0bb28d7d9c7eac6d
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-10-31 18:31:20 +0530

    fall back img

--- src/components/CounterDisplay.jsx
+++ src/components/CounterDisplay.jsx
@@ -1,12 +1,12 @@
 import useCounterStore from "./../store/useCounterStore/useCounterStore";
-
+import FallBackImg from './../assets/image.png'
 export default function CounterDisplay() {
     const { count, image } = useCounterStore();
     return (
         <div className="p-4 card rounded-lg border-amber-100">
             <h2 className="text-yellow-400">Shared Count: {count}</h2>
             image at component path /components/CounterDisplay.jsx
-            <img className="card-img-top w-52 h-52 mx-auto rounded-lg border-amber-100" src={image} alt="testing 1" />
+            <img className="card-img-top w-52 h-52 mx-auto rounded-lg border-amber-100 object-cover" src={image || FallBackImg} alt="testing 1" />
         </div>
     );
 }
--- src/components/testing1/Testing.jsx
+++ src/components/testing1/Testing.jsx
@@ -1,11 +1,11 @@
 import useCounterStore from '../../store/useCounterStore/useCounterStore';
-
+import FallBackImg from './../../assets/image.png'
 const Testing = () => {
     const { count, image } = useCounterStore();
     return (
         <div className="p-4 card rounded-lg border-amber-100">
             <h2 className="text-yellow-400">testing image at component path /components/testing1/testing.jsx </h2>
-            <img className="card-img-top w-26 h-26 mx-auto rounded-lg border-amber-100" src={image} alt="testing 1" />
+            <img className="card-img-top w-26 h-26 mx-auto rounded-lg border-amber-100 object-cover" src={image || FallBackImg} alt="testing 1" />
         </div>
     )
 }
